f84386cc3dc4003a2b35ef2d354bb79d60686c9a
David Sword (Work) <davidsword@Betsy2.local>
2019-09-16 14:10:47 -0700

    update repo

--- vip-jetpack-sync-cron.php
+++ vip-jetpack-sync-cron.php
@@ -1,13 +1,14 @@
-<?php 
+<?php
 
 /*
 Plugin Name: VIP Jetpack Sync Cron
-Description: This drop-in plugin ensures that Jetpack only syncs on the dedicated cron task.
+Description: This plugin ensures that Jetpack only syncs on the dedicated cron task.
 Version: 2.0
-Author: Rebecca Hum, Automattic 
+Author: Rebecca Hum, Automattic
 */
 
 use Automattic\Jetpack\Sync\Actions;
+use Automattic\Jetpack\Sync\Settings;
 
 if ( class_exists( 'VIP_Jetpack_Sync_Cron' ) ) {
 	return;
@@ -19,7 +20,7 @@ class VIP_Jetpack_Sync_Cron {
 
 	/**
 	 * Initiate an instance of this class if one doesn't exist already.
-	 * 
+	 *
 	 * @return VIP_Jetpack_Sync_Cron instance
 	 */
 	static public function init() {
@@ -42,19 +43,19 @@ class VIP_Jetpack_Sync_Cron {
 
 	/**
 	 * Class constructor for hooking actions/filters.
-	 * 
+	 *
 	 * @return void
 	 */
 	public function __construct() {
 		add_filter( 'cron_schedules', [ $this, 'jp_sync_cron_schedule_interval' ] );
 		add_filter( 'jetpack_sync_incremental_sync_interval', [ $this, 'filter_jetpack_sync_interval' ], 999 );
 		add_filter( 'jetpack_sync_full_sync_interval', [ $this, 'filter_jetpack_sync_interval' ], 999 );
-		add_filter( 'jetpack_sync_sender_should_load', [ 'Jetpack_Sync_Settings', 'is_doing_cron' ], 999 ); // Short circuit loading of Jetpack sender to sync only on cron.
+		add_filter( 'jetpack_sync_sender_should_load', [ Settings::class, 'is_doing_cron' ], 999 ); // Short circuit loading of Jetpack sender to sync only on cron.
 	}
 
 	/**
 	 * Filter to add custom interval to schedule.
-	 * 
+	 *
 	 * @param array  $schedules
 	 */
 	public function jp_sync_cron_schedule_interval( $schedules ) {
@@ -62,13 +63,13 @@ class VIP_Jetpack_Sync_Cron {
 		    'interval' => 60,
 		    'display'  => esc_html__( 'Every minute' ),
 		];
-		
+
 		return $schedules;
 	}
 
 	/**
 	 * Filter to return custom cron interval name.
-	 * 
+	 *
 	 * @param string  $incremental_sync_cron_schedule
 	 */
 	public function filter_jetpack_sync_interval() {
